9d8560e1b2f0a4c3b772ca6a934e2814ba5c9fe3
Robert Smith <rasmith3@sheffield.ac.uk>
2019-11-21 10:48:16 +0000

    getrr

---
+++ R/get_rr.R
@@ -0,0 +1,20 @@
+#' @title get_rr
+#'
+#' @description This package gives a relative risk for a particular MET-min level
+#'
+#' @param x
+#'
+#' @return returns a vector of relative risk of mortality
+#'
+#' @examples 
+#' get_rr(1000)
+#'
+#' @export get_rr
+
+get_rr <- function(x) {
+  
+  y <- x/1000
+  
+  return(y)
+  
+}
